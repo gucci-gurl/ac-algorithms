b18b44a7c4088c7ed7cad2893cb00917b6df12c8
GuccieGurl <guccigurl@anniecannons.com>
2018-01-16 14:14:46 -0800

    finished dashinsert.js excersise

--- dashInsert.js
+++ dashInsert.js
@@ -26,5 +26,20 @@ console.log(result) // "1-3"
 **/
 
 function dashInsert(str) {
-	// Your code
+var myArray	= number.toString().split("");
+	
+	var output = "";
+
+	for(var i = 0; i < myArray.length; i++){
+
+		if(myArray[i] % 2 === 1 && myArray[i +1] % 2 ===1){
+
+			output = output  +  myArray[i] + "-";
+		} else {
+			output = out + myArray[i];
+		}
+	}
+	console.log(output);
 }
+var result = dashInsert(454793);
+console.log(result)
